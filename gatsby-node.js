b125e3e3122de7f7841378d47fd06ae542158d8c
Ola Vea <ola@olavea.com>
2019-10-15 11:40:22 +0200

    not working yet, but getting closer to using the youtube api

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,5 +1,127 @@
-const path = require(`path`);
 const { createFilePath } = require(`gatsby-source-filesystem`);
+
+// require("now-env");
+const crypto = require("crypto");
+const ypi = require("youtube-playlist-info");
+const YT_KEY = require("./client_secrets.json")["yt_key"];
+// require("./client_secrets.json")["yt_key"];
+const playlistIds = {
+  react: "PLF8WgaD4xmjWuh7FTYTealxehOuNor_2S",
+  state: "PLF8WgaD4xmjUwRQMfDtGjJ1_UhOVMTjR9"
+};
+
+// const slugify = require("slugify");
+const path = require("path");
+
+if (process.env.NODE_ENV === "development") {
+  process.env.GATSBY_WEBPACK_PUBLICPATH = "/";
+}
+
+exports.sourceNodes = ({ actions }) => {
+  const { createNode } = actions;
+  const makeNode = node => {
+    node.internal.contentDigest = crypto
+      .createHash("md5")
+      .update(JSON.stringify(node))
+      .digest("hex");
+
+    createNode(node);
+  };
+
+  let ytNode = {
+    id: "youtube",
+    children: ["ytPlaylists"],
+    parent: null,
+    internal: {
+      type: "youtube"
+    }
+  };
+
+  let playlistsNode = {
+    id: "ytPlaylists",
+    parent: "youtube",
+    children: [],
+    internal: {
+      type: "ytPlaylists"
+    }
+  };
+
+  const basePlaylistNode = ([playlistKey, playlistId]) => ({
+    id: `ytPlaylist-${playlistId}`,
+    playlistKey: playlistKey,
+    playlistId: playlistId,
+    parent: "ytPlaylists",
+    children: [],
+    internal: {
+      type: "ytPlaylist"
+    }
+  });
+
+  return Promise.all(
+    Object.entries(playlistIds).map(async ([playlistKey, playlistId]) => {
+      const items = await ypi(YT_KEY, playlistId);
+      const playlistNode = basePlaylistNode([playlistKey, playlistId]);
+
+      playlistNode.children = items.map(
+        ({ title, description, resourceId, thumbnails, position }) => {
+          const id = `ytVideo-${resourceId.videoId}`;
+          makeNode({
+            id,
+
+            title,
+            description,
+            thumbnails,
+            position,
+            resourceId,
+            videoId: resourceId.videoId,
+            internal: {
+              type: "ytVideo"
+            },
+            parent: playlistNode.id,
+            children: []
+          });
+          return id;
+        }
+      );
+
+      makeNode(playlistNode);
+      return playlistNode.id;
+    })
+  ).then(playlistNodeIds => {
+    playlistsNode.children = playlistNodeIds;
+    makeNode(playlistsNode);
+    makeNode(ytNode);
+  });
+};
+
+// exports.onCreateNode = ({ node, getNode, actions }) => {
+//   if (node.internal.type === "MarkdownRemark") {
+//     const fileNode = getNode(node.parent);
+//     const slug =
+//       fileNode.relativeDirectory +
+//       "/" +
+//       slugify(node.frontmatter.title, { lower: true });
+
+//     actions.createNodeField({
+//       node,
+//       name: "pagePath",
+//       value: "/" + slug
+//     });
+//   }
+// };
+
+exports.onCreateNode = ({ node, actions, getNode }) => {
+  const { createNodeField } = actions;
+
+  if (node.internal.type === `MarkdownRemark`) {
+    const value = createFilePath({ node, getNode });
+    createNodeField({
+      name: `slug`,
+      node,
+      value
+    });
+  }
+};
 
 exports.createPages = ({ graphql, actions }) => {
   const { createPage } = actions;
@@ -13,6 +135,9 @@ exports.createPages = ({ graphql, actions }) => {
         allMarkdownRemark(limit: 1000) {
           edges {
             node {
+              frontmatter {
+                title
+              }
               fields {
                 slug
               }
@@ -29,31 +154,27 @@ exports.createPages = ({ graphql, actions }) => {
     // Create blog posts pages.
     const posts = result.data.allMarkdownRemark.edges;
 
-    posts.forEach(post => {
+    posts.forEach((post, articles, index) => {
       createPage({
         path: post.node.fields.slug,
         component: pageTemplate,
         context: {
           slug: post.node.fields.slug
+          // videoId: node.frontmatter.videoId,
+          // prevPagePath:
+          //   articles[index - 1] && articles[index - 1].node.fields.slug,
+          // nextPagePath:
+          //   articles[index + 1] && articles[index + 1].node.fields.slug,
+          // prevTitle:
+          //   articles[index - 1] && articles[index - 1].node.frontmatter.title,
+          // nextTitle:
+          //   articles[index + 1] && articles[index + 1].node.frontmatter.title
         }
       });
     });
 
     return null;
   });
-};
-
-exports.onCreateNode = ({ node, actions, getNode }) => {
-  const { createNodeField } = actions;
-
-  if (node.internal.type === `MarkdownRemark`) {
-    const value = createFilePath({ node, getNode });
-    createNodeField({
-      name: `slug`,
-      node,
-      value
-    });
-  }
 };
 
 // Why not       value: slug, Like in :
---
+++ src/Curriiculum.js
@@ -0,0 +1,14 @@
+// import React from "react";
+
+// import { Row, Col as Column } from "react-bootstrap";
+
+// export const Curriculum = ({ videos, articles, small }) => (
+//   <Row
+//         {videos.map(video => (
+//           <Col
+//             video={video}
+//             article={articles.get(video.videoId)}
+//             key={video.id}
+//           />
+
+// );
